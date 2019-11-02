34084e7be86da59f93909082fc6cee9285012e45
ajreid <axr490@miami.edu>
2019-11-02 14:16:59 -0400

    initialized .html file, linked resources to be used (firebase, bootstrap, .js & .css)

---
+++ assets/javascript/main.js
@@ -0,0 +1,16 @@
+  var firebaseConfig = {
+    apiKey: "AIzaSyA6pCTHavcLWKgngWrzWBOLcUXTveCDD9Y",
+    authDomain: "rps-multiplayer-7d6d7.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-7d6d7.firebaseio.com",
+    projectId: "rps-multiplayer-7d6d7",
+    storageBucket: "rps-multiplayer-7d6d7.appspot.com",
+    messagingSenderId: "337347504629",
+    appId: "1:337347504629:web:7707bf3251b547a3b758bc"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  $(document).ready(function()
+  {
+      alert("hey");
+  })
